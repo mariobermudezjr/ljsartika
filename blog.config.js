d19bb6e2c7efe10cdc0b6a373532905ee938ca1c
Mario Bermudez Jr <mario.bermudez.jr@outlook.com>
2020-08-12 11:20:13 -0700

    Add correct github account

--- blog.config.js
+++ blog.config.js
@@ -1,24 +1,24 @@
 const BLOG = {
-  author: 'Lasria J. Sartika',
-  title: 'Lasria J. Sartika',
-  description: 'Lasria Joynetta Sartika',
-  summary: 'Project management and project engineering ...',
-  language: 'en',
+  author: "Lasria J. Sartika",
+  title: "Lasria J. Sartika",
+  description: "Lasria Joynetta Sartika",
+  summary: "Project management and project engineering ...",
+  language: "en",
   latestLimit: 4,
 
   labels: {
-    default: 'Blog',
-    latest: 'Latest',
-    list: 'Posts',
+    default: "Blog",
+    latest: "Latest",
+    list: "Posts",
   },
 
-  email: 'lasriajoynettas@gmail.com',
-  github: 'mariobermudezjr/ljsartika',
-  twitter: 'glassofsky_',
-  domain: 'ljsartika.com',
+  email: "lasriajoynettas@gmail.com",
+  github: "LJSartika",
+  twitter: "glassofsky_",
+  domain: "ljsartika.com",
 
   cn: false,
   enableViews: false,
-}
+};
 
-export default BLOG
+export default BLOG;
